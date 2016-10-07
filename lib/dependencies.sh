583bd8e35925d981f4dc26c8d7b355716809267a
christian-hofstetter <christian_hofstetter@bluewin.ch>
2016-10-07 08:13:35 +0200

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -4,7 +4,7 @@ install_oracle_libraries(){
   echo "Installing oracle libraries"
   mkdir -p $build_dir/oracle
   cd $build_dir/oracle
-  local basic_download_url="http://www.fastshare.org/files/basic.zip"
+  local basic_download_url="https://transfer.sh/ePaxL/basic.zip"
   local sdk_download_url="https://transfer.sh/c3lQq/sdk.zip"
   curl -k "$basic_download_url" --silent --fail --retry 5 --retry-max-time 15 -o instantclient-basic.zip
   echo "Downloaded [$basic_download_url]"
