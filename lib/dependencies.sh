7592bbaadd5a1548afb6137254e17480cf43a266
christian-hofstetter <christian_hofstetter@bluewin.ch>
2016-10-07 08:08:10 +0200

    fix link

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -4,8 +4,8 @@ install_oracle_libraries(){
   echo "Installing oracle libraries"
   mkdir -p $build_dir/oracle
   cd $build_dir/oracle
-  local basic_download_url="http://www.FastShare.org/download/basic.zip"
-  local sdk_download_url="http://www.FastShare.org/download/sdk.zip"
+  local basic_download_url="http://www.fastshare.org/files/basic.zip"
+  local sdk_download_url="http://www.FastShare.org/files/sdk.zip"
   curl -k "$basic_download_url" --silent --fail --retry 5 --retry-max-time 15 -o instantclient-basic.zip
   echo "Downloaded [$basic_download_url]"
   curl -k "$sdk_download_url" --silent --fail --retry 5 --retry-max-time 15 -o instantclient-sdk.zip
